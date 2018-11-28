57798181ebc548c11f8348f14c095f17a1f97c34
jay51 <nenojaafar3@gmail.com>
2018-11-27 20:36:27 -0600

    added login and register templates

--- routes/auth.js
+++ routes/auth.js
@@ -3,23 +3,30 @@ const router = express.Router();
 
 router.get("/login", function(req, res) {
 	// return login form
-	res.send("login Form");
+	res.render("auth/login_form");
 });
 
 router.post("/login", function(req, res) {
+	const { email, password } = req.body;
 	// check hashed password
 	// set the session
+	console.log(email, password);
+	res.send(`${email}, ${password}`);
 });
 
 router.get("/register", function(req, res) {
 	// return register form
-	res.send("register form");
+	res.render("auth/register");
 });
 
 router.post("/register", function(req, res) {
+	const { email, password, confirm_password, first_name, last_name } = req.body;
 	// encrypt password
 	// create user
 	// set the session & redirect to page
+	res.send(
+		`${email}, ${password}, ${confirm_password}, ${first_name}, ${last_name}`
+	);
 });
 
 router.get("/logout", function(req, res) {
